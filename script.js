756b7b46a303f860aee8dd74cef46442a63de78c
Berckson Thierry <abreuthierry3@gmail.com>
2024-11-22 10:40:42 -0300

    script.js

--- script.js
+++ script.js
@@ -1,54 +1,63 @@
-alert("Bem vindo ao meu projeto pessoal ")
-alert('como vai fucionar?')
+alert("Bem-vindo ao meu projeto pessoal")
+alert("Como vai funcionar?")
 alert(`Projeto Pessoal: X1 de Basquete
 
 O jogo consiste em um duelo de 1 contra 1, onde um jogador ataca e o outro defende. Durante o ataque, o jogador tem a opção de fazer uma bandeja ou realizar um arremesso.
 
 Regras:
 
-O jogador tem 10 tentativas ou 15 pontos para vencer.
-Ao final do jogo, serão mostradas as seguintes estatísticas:
-°Número de tentativas realizadas.
-°Quantidade de pontos marcados.
-°Média de pontos por tentativa.
-°Número de tocos recebidos.
-°Quantidade de arremessos errados (air balls).` )
- alert("vamos começar")
+- O jogador tem 10 tentativas ou 15 pontos para vencer.
+- Ao final do jogo, serão mostradas as seguintes estatísticas:
+  ° Número de tentativas realizadas.
+  ° Quantidade de pontos marcados.
+  ° Média de pontos por tentativa.
+  ° Número de tocos recebidos.
+  ° Quantidade de arremessos errados (air balls).`)
+
+alert("Vamos começar!")
+
 let hub = ''
-let ply1 = prompt("qual nome do seu jogador que vai atacar?")
-let ply2 = prompt("qual nome do seu jogador que vai defender?")
+let ply1 = prompt("Qual o nome do seu jogador que vai atacar?")
+let ply2 = prompt("Qual o nome do seu jogador que vai defender?")
+
 function randomNum(n) {
     return Math.floor(Math.random() * (n + 1))
 }
+
 let pontos = 0
 let tentativas = 0
 let limitePontos = 15
-let limitedetentativas = 10
+let limiteTentativas = 10
 let toco = 0
 let airBall = 0
 
-
-for(let i = 0; tentativas < limitedetentativas && pontos <= limitePontos; i++) {
+for (let i = 0; tentativas < limiteTentativas && pontos <= limitePontos; i++) {
     let resultado = randomNum(5)
     tentativas++
-    hub = prompt("1: voce quer fazer bandeja\n2: voce quer arremesar ")
+
+    hub = prompt("1: Você quer fazer bandeja\n2: Você quer arremessar")
+
     if (hub == 1) {
-        if(resultado < 4) {
-            alert(`${ply1} fez bandeja e fez o ponto`)
+        if (resultado < 4) {
+            alert(`${ply1} fez bandeja e marcou o ponto!`)
             pontos += 2
-        } else if (resultado > 3) {
-            alert(`${ply2} Deu o toco em ${ply1}`)
+        } else {
+            alert(`${ply2} deu o toco em ${ply1}!`)
             toco += 1
         }
     } else if (hub == 2) {
         if (resultado < 4) {
-            alert(`${ply1} fez um arremeso e fez o ponto`)
+            alert(`${ply1} fez um arremesso e marcou o ponto!`)
             pontos += 3
-        } else if (resultado > 3) {
-            alert(`${ply1} arremessou e deu AIR BALL`)
+        } else {
+            alert(`${ply1} arremessou e deu AIR BALL!`)
             airBall += 1
         }
     }
 }
-alert(`Você teve ${tentativas} tentativas fez ${pontos} pontos e teve ${pontos/tentativas} pontos por tentativa e levou ${toco} tocos de ${ply2} e deu ${airBall} air ball`)
+
+alert(`Você teve ${tentativas} tentativas, fez ${pontos} pontos, e teve uma média de ${pontos / tentativas} pontos por tentativa.
+Você levou ${toco} tocos de ${ply2} e deu ${airBall} air balls.`)
+
+alert("Obrigado por jogar!")  
 
